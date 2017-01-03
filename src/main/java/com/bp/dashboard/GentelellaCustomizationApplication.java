5192e63c14909beb3abf5073dff3090b5b9985ef
Pirvali, Behzad <bpirvali@gmail.com>
2017-01-03 11:43:28 -0800

    initial commit

---
+++ src/main/java/com/bp/dashboard/GentelellaCustomizationApplication.java
@@ -0,0 +1,12 @@
+package com.bp.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentelellaCustomizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentelellaCustomizationApplication.class, args);
+	}
+}
